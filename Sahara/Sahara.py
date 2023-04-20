6160be2aca28cb5ff97330a918276f734cd56fa2
PpNutthanon <nutthanon.hw@gmail.com>
2023-04-20 18:11:11 +0700

    -

--- Sahara/Sahara.py
+++ Sahara/Sahara.py
@@ -1,71 +1,56 @@
 # Import excel sheet into python
 import pandas as pd
 df = pd.read_excel(r"Sahara\TableA17.xlsx")
-#print(df)
 
-#* Declare variables for calculation
+#Todo: Declare variables for calculation
 variables = ["t", "h", "pr", "u", "vr", "s"]
 print("---Enter variables input (number)---")
-print(" [1] : T ","\n","[2] : H ","\n","[3] : Pr","\n","[4] : u","\n","[5] : vr","\n","[6] : s")
+for i, var in enumerate(variables, 1):
+    print(f" [{i}] : {var.upper()}")
+
+#Todo: Create function in order to check the condition 
+def find_values(var_index, input_value):
+    sorted_df = df.sort_values(by=variables[var_index-1])
+    
+    less_than_mask = sorted_df[variables[var_index-1]] < input_value
+    greater_than_mask = sorted_df[variables[var_index-1]] > input_value
+    
+    if less_than_mask.any(): #* Bring Boolean:True in less_than_mask for calculation
+        closest_less_than = sorted_df[less_than_mask].iloc[-1] 
+    else:
+        closest_less_than = None
+        
+    if greater_than_mask.any():
+        closest_greater_than = sorted_df[greater_than_mask].iloc[0]
+    else:
+        closest_greater_than = None
+        
+    exact_match = sorted_df[sorted_df[variables[var_index-1]] == input_value]
+    return exact_match, closest_less_than, closest_greater_than
+
 data = int(input("Enter variables you want to input: "))
+value = float(input(f"Enter {variables[data-1].upper()} value: "))
 
-
-#* Check conditions which variable was selected
-if data == 1: 
-    t = float(input("Enter T value: "))
-    values = df.loc[df[variables[data-1]] == t]
-    if len(values) == 0:
-        print("Required Equation")
-        #Todo: เอาสูตรเข้ามาคำนวณ
+#Todo: Show the data and also interpoled calculation when the value not exactly the same in the excel sheet
+if 1 <= data <= 6:
+    exact_values, values_less, values_greater = find_values(data, value)
+    
+    if len(exact_values) == 0:
+        print("No exact match found. Closest values:")
+        print("\nRow before:", values_less)
+        print("\nRow after:", values_greater)
+        values_less   = values_less.tolist()
+        values_greater = values_greater.tolist()
+        interpolated_values = []
+        ratio = (value - values_less[data-1])/(values_greater[data-1] - values_less[data-1])
+        for i in range(len(variables)):
+            interpolated = values_less[i] + (ratio * (values_greater[i] - values_less[i]))
+            interpolated_values.append(round(interpolated,3))
+        print(interpolated_values)
     else:
-        print(values)
-        #Todo ลบให้output ไม่แสดง row 
-
-
-elif data == 2:
-    h = float(input("Enter h value: "))
-    values = df.loc[df[variables[data-1]] == h]
-    if len(values) == 0:
-        print("Required Equation")
-    else:
-        print(values)
-
-
-elif data == 3: 
-    pr = float(input("Enter Pr value: "))
-    values = df.loc[df[variables[data-1]] == pr]
-    if len(values) == 0:
-        print("Required Equation")
-    else:
-        print(values)
-
-
-elif data == 4: 
-    u = float(input("Enter u value: "))
-    values = df.loc[df[variables[data-1]] == u]
-    if len(values) == 0:
-        print("Required Equation")
-    else:
-        print(values)
-
-
-elif data == 5: 
-    vr = float(input("Enter Vr value: "))
-    values = df.loc[df[variables[data-1]] == vr]
-    if len(values) == 0:
-        print("Required Equation")
-    else:
-        print(values)
-
-
-elif data == 6: 
-    s = float(input("Enter s value: "))
-    values = df.loc[df[variables[data-1]] == s]
-    if len(values) == 0:
-        print("Required Equation")
-    else:
-        print(values)
-else: 
+        print("Exact match found:")
+        print(exact_values)
+else:
     print("Wrong input")
 
 print(variables)
